8f74fd129d61b05e877385095dd681d63484b57b
Serhii Saunin <serhiisaunin@gmail.com>
2021-09-21 04:01:36 +0300

    fix bags

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 const { src, dest, parallel, series, watch } = require("gulp");
 
-const browserSync = require("browser-sync").create(),
-  sass = require("gulp-sass")(require("sass")),
-  concat = require("gulp-concat"),
-  autoprefixer = require("gulp-autoprefixer"),
-  uglify = require("gulp-uglify-es").default,
-  imagemin = require("gulp-imagemin"),
-  changed = require("gulp-changed"),
-  del = require("del");
+const browserSync   = require("browser-sync").create(),
+      plumber       = require("gulp-plumber"),
+      sass          = require("gulp-sass")(require("sass")),
+      concat        = require("gulp-concat"),
+      autoprefixer  = require("gulp-autoprefixer"),
+      uglify        = require("gulp-uglify-es").default,
+      imagemin      = require("gulp-imagemin"),
+      image         = require('gulp-image'),
+      changed       = require("gulp-changed"),
+      del           = require("del");
 
 const server = () => {
   browserSync.init({
@@ -19,6 +21,7 @@ const server = () => {
 
 const styles = () => {
   return src("app/scss/**/*.scss")
+    .pipe(plumber())
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(concat("style.min.css"))
     .pipe(
@@ -29,14 +32,15 @@ const styles = () => {
 };
 
 const scripts = () => {
-  return src("app/js/src/script.js")
+  return src("app/js/src/*.js")
+    .pipe(plumber())
     .pipe(concat("script.min.js"))
     .pipe(uglify())
     .pipe(dest("app/js/dest"))
     .pipe(browserSync.stream());
 };
 
-const images = () => {
+const img = () => {
   return src("app/img/src/**/*")
     .pipe(changed("app/img/dest"))
     .pipe(
@@ -49,6 +53,9 @@ const images = () => {
         }),
       ])
     )
+    .pipe(image({
+      svgo: false
+    }))
     .pipe(dest("app/img/dest"));
 };
 
@@ -56,7 +63,7 @@ const cleanImg = () => {
   return del("app/img/dest/**/*", { force: true });
 };
 
-const buildDist = () => {
+const distBuild = () => {
   return src(
     [
       "app/css/**/*.min.css",
@@ -73,18 +80,18 @@ const cleanBuild = () => {
 };
 
 const watcher = () => {
-  watch(["app/scss/**/*.js", "!app/**/*.min.js"], scripts);
+  watch(["app/js/**/*.js", "!app/**/*.min.js"], scripts);
   watch("app/**/*.scss", styles);
   watch("app/**/*.html").on("change", browserSync.reload);
-  watch("app/img/src/**/*", images);
+  watch("app/img/src/**/*", img);
 };
 
-exports.server = server;
-exports.scripts = scripts;
-exports.styles = styles;
-exports.images = images;
-exports.buildDist = buildDist;
-exports.cleanImg = cleanImg;
-exports.cleanBuild = cleanBuild;
-exports.build = series(cleanBuild, styles, scripts, images, buildDist);
-exports.default = parallel(server, styles, scripts, watcher);
+exports.server      = server;
+exports.scripts     = scripts;
+exports.styles      = styles;
+exports.img         = img;
+exports.distBuild   = distBuild;
+exports.cleanImg    = cleanImg;
+exports.cleanBuild  = cleanBuild;
+exports.build       = series(cleanBuild, styles, scripts, img, distBuild);
+exports.default     = parallel(server, styles, scripts, watcher);
