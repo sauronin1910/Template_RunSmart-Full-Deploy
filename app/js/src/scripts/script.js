258d60d1b23a5dcb9b87ada56d9c0e7c38a632e3
Serhii Saunin <serhiisaunin@gmail.com>
2021-09-23 20:12:12 +0300

    Add Swiper

--- app/js/src/scripts/script.js
+++ app/js/src/scripts/script.js
@@ -1,1 +1,21 @@
-const swiper = new Swiper('.swiper'); 
+const swiper = new Swiper(".carousel__container", {
+  autoHeight: true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".carousel__dots",
+    bulletClass: "carousel__dot-item swiper-pagination-bullet",
+    bulletActiveClass: "swiper-pagination-bullet-active",
+    bulletElement: "span",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: false,
+  navigation: {
+    nextEl: ".carousel__btn-next",
+    prevEl: ".carousel__btn-prev",
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
