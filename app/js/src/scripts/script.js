b97cf76db2b5b0d00029e600506cd7c2d53bad36
Serhii Saunin <serhiisaunin@gmail.com>
2021-09-24 04:15:36 +0300

    Add Tabs

--- app/js/src/scripts/script.js
+++ app/js/src/scripts/script.js
@@ -2,7 +2,6 @@ const swiper = new Swiper(".carousel__container", {
   // autoHeight: true,
   spaceBetween: 20,
   loop: true,
-  //   loopFillGroupWithBlank: true,
   pagination: {
     el: ".carousel__dots",
     bulletClass: "carousel__dot-item swiper-pagination-bullet",
@@ -20,4 +19,41 @@ const swiper = new Swiper(".carousel__container", {
     delay: 2500,
     disableOnInteraction: false,
   },
+});
+
+$(function() {
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+  
+  $(".catalog-item__link").each(function(i) {
+    $(this).on("click", function(e) {
+      e.preventDefault();
+      $(".catalog-item__content ").eq(i).toggleClass("catalog-item__content_active");
+      $(".catalog-item__list ").eq(i).toggleClass("catalog-item__list_active")
+    })
+  });
+  $(".catalog-item__back").each(function(i) {
+    $(this).on("click", function(e) {
+      e.preventDefault();
+      $(".catalog-item__content ").eq(i).toggleClass("catalog-item__content_active");
+      $(".catalog-item__list ").eq(i).toggleClass("catalog-item__list_active")
+    })
+  });
+
+
+// function toggleSlide(item) {
+//   $(item).each(function(i) {
+//     $(this).on("click", function(e) {
+//       e.preventDefault();
+//       $(".catalog-item__content ").eq(i).toggleClass("catalog-item__content_active");
+//       $(".catalog-item__list ").eq(i).toggleClass("catalog-item__list_active")
+//     })
+//   });
+// };
+//   toggleSlide(".catalog-item__link");
+//   toggleSlide(".catalog-item__back");
+
 });
