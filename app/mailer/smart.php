0b22d0db881f15c818e716c47d6cd42a157d9eca
Serhii Saunin <serhiisaunin@gmail.com>
2021-10-02 23:19:04 +0300

    minor fix php

--- app/mailer/smart.php
+++ app/mailer/smart.php
@@ -14,12 +14,12 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'hiwan17978@cantouri.com';                 // Наш логин
-$mail->Password = 'nehysrvty2013';                           // Наш пароль от ящика
+$mail->Password = 'asdf3223';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 
 $mail->setFrom('', 'Pulse');   // От кого письмо
-$mail->addAddress('hiwan17978@cantouri.com');     // Add a recipient
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
